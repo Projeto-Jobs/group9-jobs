a8ebb73e7f057413623668e654b49712f54b1749
tsunodajapa <gabrieltsunoda@gmail.com>
2023-07-07 16:05:37 -0300

    fix: provider unicos

--- src/components/ProtectedRoutes/index.tsx
+++ src/components/ProtectedRoutes/index.tsx
@@ -1,9 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom"
+import { AdminProvider } from "../../providers/AdminContext"
+import { AdmListProvider } from "../../providers/AdmListContext"
 
 
 export const ProtectedRoutes = () =>{
     const token = localStorage.getItem("@Jobs:token")
     
-    return token ? <Outlet /> : <Navigate to="/"/>
+    return token ? 
+    <AdminProvider>
+        <AdmListProvider>
+            <Outlet />
+        </AdmListProvider>
+    </AdminProvider>
+    :
+     <Navigate to="/"/>
 }
 
--- src/routes/RoutesMain.tsx
+++ src/routes/RoutesMain.tsx
@@ -9,19 +9,13 @@ import { ApplicationProvider } from "../providers/ModalContext";
 import { LoginProvider } from "../providers/LoginContext";
 import { ProtectedRoutes } from "../components/ProtectedRoutes";
 import { PublicRoutes } from "../components/PublicRoutes";
-<<<<<<< HEAD
 import { AdmListProvider } from "../providers/AdmListContext";
 import { CreateVacancyPage } from "../pages/AdmCreateVacancyPage";
 import { AdminProvider } from "../providers/AdminContext";
 import { EditVacancys } from "../components/AdmList/editVacancys";
-=======
-import { AdmJobListContext } from "../providers/AdmListContext";
-import { CreateVacancyPage } from "../pages/AdmCreateVacancyPage";
-import { AdminProvider } from "../providers/AdminContext";
->>>>>>> 05fabc05fb2132bc834d7713c8df0d2b355ad81f
 
 export const RoutesMain = () => {
-  
+
   return (
     <Routes>
       <Route element={<PublicRoutes />}>
@@ -42,64 +36,36 @@ export const RoutesMain = () => {
               <RegisterPage />
             </RegisterProvider>
           }
-          />
-        </Route>
-        
-        <Route element={<ProtectedRoutes/>}>
-          <Route path="/CreateVacancyPage" element={
-          <AdminProvider>
-            <CreateVacancyPage/>
-          </AdminProvider>}/>
-
-          <Route path="EditPage" element={<AdmListProvider>
-          <EditVacancys/>
-          </AdmListProvider>} />
-          
-          <Route path="/CreateVacancyPage" element={
-          <AdminProvider>
-            <CreateVacancyPage/>
-          </AdminProvider>}/>
-          
-          <Route 
-          path="/AdminPage" 
-          element={
-            <LoginProvider>
-              <AdmListProvider>
-                <Administration/>
-                
-              </AdmListProvider>
-            </LoginProvider>
         />
       </Route>
 
       <Route element={<ProtectedRoutes />}>
-        <Route
-          path="/CreateVacancyPage"
-          element={
-            <AdminProvider>
-              <CreateVacancyPage />
-            </AdminProvider>
-          }
-        />
-
-        <Route
-          path="/CreateVacancyPage"
-          element={
-            <AdminProvider>
-              <CreateVacancyPage />
-            </AdminProvider>
-          }
-        />
-
+        <Route path="/CreateVacancyPage" element={<CreateVacancyPage />} />
+        <Route path="EditPage" element={<EditVacancys />} />
+        <Route path="/CreateVacancyPage" element={<CreateVacancyPage />} />
         <Route
           path="/AdminPage"
           element={
-              <AdmJobListContext>
+            <LoginProvider>
                 <Administration />
-              </AdmJobListContext>
+            </LoginProvider>
+          }
+        />
+        <Route
+          path="/CreateVacancyPage"
+          element={
+            <CreateVacancyPage />
+          }
+        />
+
+        <Route
+          path="/CreateVacancyPage"
+          element={
+            <CreateVacancyPage />
           }
         />
       </Route>
+
     </Routes>
   );
 };
