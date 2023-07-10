8d9db01a74991a06c80f2489187bf6edd999d4db
kelly <nadjakelly2020@gmail.com>
2023-07-09 22:23:19 -0300

    STYLE: Página de login estilizada

--- src/components/AdmList/editVacancys.tsx
+++ src/components/AdmList/editVacancys.tsx
@@ -4,6 +4,11 @@ import { InputField } from "../InputField"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { SchemaEdit } from "./editSchema"
+import { FormStyled } from "./styledForm"
+import { StyledTitle2, StyledTitle3 } from "../../styles/Typography"
+import { StyledButton } from "../../styles/Global"
+import { AiOutlineArrowLeft } from "react-icons/ai"
+import { Link } from "react-router-dom"
 
 type TEdit = z.infer<typeof SchemaEdit>
 
@@ -15,10 +20,10 @@ export const EditVacancys = () => {
 
   const submit = (formData: TEdit) => {
     const userId = Number(localStorage.getItem("@Jobs:userId"))
-    const { id, ...data } = formData; 
+    const { id, ...data } = formData;
     const updatedData: IAdmJob = {
       userId,
-      id: id, 
+      id: id,
       ...data,
     }
     editVacanciesJob(updatedData)
@@ -26,24 +31,31 @@ export const EditVacancys = () => {
 
 
   return (
-    <div>
-      <h2>Editando: </h2>
+    <FormStyled>
+      <Link to="/">
+      <StyledTitle3 color= "blue" className="return"> <AiOutlineArrowLeft/> Voltar</StyledTitle3>
+      </Link>
+      <StyledTitle2 color= "blue" >Editando: </StyledTitle2>
       <form onSubmit={handleSubmit(submit)} >
-        <InputField
-          type="text"
-
-          {...register("position")}
-        />
-        <InputField type="number"
-
-          {...register("sallary")}
-        />
-        <textarea
-          {...register("description")}
-        ></textarea>
-        <button type="submit">Atualizar</button>
-
+       <div className="input">
+          <InputField
+            type="text"
+            placeholder="Cargo"
+            {...register("position")}
+          />
+          <InputField type="number"
+            placeholder="Salário (opcional)"
+            {...register("sallary")}
+          />
+        </div>
+        <div className="textarea">
+          <textarea
+            placeholder="Descrição"
+            {...register("description")}>
+          </textarea>
+        </div>
       </form>
-    </div>
+        <StyledButton type="submit">Atualizar</StyledButton>
+    </FormStyled>
   )
 }
